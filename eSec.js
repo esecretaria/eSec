a8a63c29299d5e89815fbcb0b37ec6273befa337
esecretaria <43152343+esecretaria@users.noreply.github.com>
2019-07-11 15:59:57 -0600

    Update eSec.js

--- eSec.js
+++ eSec.js
@@ -1,10 +1,16 @@
 function hideShowPunch(valuesToHide){
   var x = valuesToHide;
-  var y = [x];
+  
 
   for(var i=0; i<=y.length; i++){
-    hidePunch(y[i]);
+    hidePunch(x[i]);
   }
+
+  if ( $ ("div.survey-body > #primary > div").hasClass("survey-error") ) {  
+      for(var i=0; i<=y.length; i++){
+        showPunch(x[i]);
+      }
+    }  
 }
 
 
