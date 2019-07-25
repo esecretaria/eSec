74a10ae5f4dcf143a8d9b20756f344a615df4b22
esecretaria <43152343+esecretaria@users.noreply.github.com>
2019-07-25 17:30:58 -0600

    Update eSec.js

--- eSec.js
+++ eSec.js
@@ -1,3 +1,35 @@
+function hidePunch2(numOfPunchToHide) {
+  
+  var x = numOfPunchToHide;
+  var nthChild = x + 2;
+  //var z = $ (".answers > .groupingCols:nth-child("+nthChild+")");
+  
+  var span = document.getElementByClass('groupingCols');
+  var row = span.getElementsByTagName('div')[nthChild].childNodes[nthChild];
+  
+  //console.log(x);
+  //console.log(nthChild);
+  //z.hide();
+  //return z;    
+   row.hide();
+  return row;
+}  
+
+function hideShowPunch(valuesToHide){
+  var x = [valuesToHide];
+  
+
+  for(var i=0; i<=x.length; i++){
+    hidePunch2(x[i]);
+  }
+
+  if ( $ ("div.survey-body > #primary > div").hasClass("survey-error") ) {  
+      for(var i=0; i<=x.length; i++){
+        showPunch(x[i]);
+      }
+    }  
+}
+
 function hidePunch(numOfPunchToHide) {
   
   var x = numOfPunchToHide;
@@ -22,20 +54,7 @@ function showPunch(numOfPunchToShow) {
   return z;    
 } 
 
-function hideShowPunch(valuesToHide){
-  var x = [valuesToHide];
-  
 
-  for(var i=0; i<=x.length; i++){
-    hidePunch(x[i]);
-  }
-
-  if ( $ ("div.survey-body > #primary > div").hasClass("survey-error") ) {  
-      for(var i=0; i<=x.length; i++){
-        showPunch(x[i]);
-      }
-    }  
-}
 
 
 
