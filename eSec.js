79b7dea2f06763b1c4849825de157b73d72c70b4
esecretaria <43152343+esecretaria@users.noreply.github.com>
2019-07-26 10:47:01 -0600

    Update eSec.js

--- eSec.js
+++ eSec.js
@@ -1,28 +1,3 @@
-function hidePunch2(numOfPunchToHide) {
-  var x = numOfPunchToHide;
-  var nthChild = x + 2;
-  var z = document.getElementByClassName("answers-list").childNodes;
-  
-  z[nthChild].style.visibility = "hidden";
-  
-  return z;
-}  
-
-function hideShowPunch(valuesToHide){
-  var x = [valuesToHide];
-  
-
-  for(var i=0; i<=x.length; i++){
-    hidePunch2(x[i]);
-  }
-
-  if ( $ ("div.survey-body > #primary > div").hasClass("survey-error") ) {  
-      for(var i=0; i<=x.length; i++){
-        showPunch(x[i]);
-      }
-    }  
-}
-
 function hidePunch(numOfPunchToHide) {
   
   var x = numOfPunchToHide;
@@ -46,9 +21,6 @@ function showPunch(numOfPunchToShow) {
   z.show();
   return z;    
 } 
-
-
-
 
 
 
